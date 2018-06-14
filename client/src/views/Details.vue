f7e773357f6bb0885f83b93406ee0d569c29dd22
Nassgr <anas.grina@hotmail.fr>
2018-06-14 09:48:22 +0200

    fonts and details view

--- client/src/views/Details.vue
+++ client/src/views/Details.vue
@@ -1,11 +1,18 @@
 <template>
-    <section>
-        <div class="body-text">
-            <div class="">
-
+        <section class="details-container">
+            <div class="text-art">
+                <div class="title-artwork title is-2">
+                    <h1>Titre de l'oeuvre</h1>
+                </div>
+                <div class="author-artwork">
+                    <h2>Nom de l'auteur</h2>
+                </div>
+                <div class="description-artwork">
+                    <p>Description de l'oeuvre</p>
+                </div>
             </div>
-        </div>    
-    </section>
+            <div class="picture-art"></div>
+        </section>
 </template>
 
 <script>
