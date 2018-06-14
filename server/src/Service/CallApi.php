bb5ea71ba7e38d56acbed384e509085f8f956851
Edwin Gerard <edwin.gerard1@gmail.com>
2018-06-14 11:53:50 +0200

    ajout if isset

--- server/src/Service/CallApi.php
+++ server/src/Service/CallApi.php
@@ -15,23 +15,51 @@ class CallApi
      * @param int $id
      * @return mixed
      */
-    public function connect(int $id)
+    public function connect (int $id)
     {
         $curlService = new CurlService();
         $resf = $curlService->generateAPiUrl('works', $id);
         $res = $resf->hits->hits[ 0 ]->_source;
-        $work[ 'apiId' ] = $res->id;
-        $work[ 'collection' ] = $res->collections[ 0 ]->name->fr;
-        $work[ 'periodStart' ] = $res->periods[ 0 ]->suggest_fr->input;
-        $work[ 'periodEnd' ] = $res->periods[ 0 ]->suggest_fr->output;
+        if (isset($res->id)) {
+            $work[ 'apiId' ] = $res->id;
+
+        }
+        if (isset($res->collections[ 0 ]->name->fr)) {
+            $work[ 'collection' ] = $res->collections[ 0 ]->name->fr;
+
+        }
+        if (isset($res->periods[ 0 ]->suggest_fr->input)) {
+            $work[ 'periodStart' ] = $res->periods[ 0 ]->suggest_fr->input;
+
+        }
+        if (isset($res->periods[ 0 ]->suggest_fr->output)) {
+            $work[ 'periodEnd' ] = $res->periods[ 0 ]->suggest_fr->output;
+
+        }
+        if (isset($res->location->name->fr)) {
+            $work[ 'locationName' ] = $res->location->name->fr;
+
+        }
+        if (isset($res->location->city->fr)) {
+            $work[ 'locationCity' ] = $res->location->city->fr;
+
+        }
+        if (isset($res->images[ 0 ]->urls->huge->url)) {
+            $work[ 'image' ] = $res->images[ 0 ]->urls->huge->url;
+
+        }
+        if (isset($res->title->fr)) {
+            $work[ 'title' ] = $res->title->fr;
+
+        }
+        if (isset($res->authors[ 0 ]->name->fr)) {
+            $authorName = $res->authors[ 0 ]->name->fr;
+
+        }
         $work[ 'technique' ] = '';
         if (isset($res->techniques[ 0 ])) {
             $work[ 'technique' ] = $res->techniques[ 0 ]->suggest_fr->input;
         }
-        $work[ 'locationName' ] = $res->location->name->fr;
-        $work[ 'locationCity' ] = $res->location->city->fr;
-        $work[ 'image' ] = $res->images[ 0 ]->urls->huge->url;
-        $work[ 'title' ] = $res->title->fr;
         $work[ 'description' ] = '';
         $work[ 'descriptionUrl' ] = '';
         if (isset($res->wikipedia_extract->fr)) {
@@ -42,11 +70,10 @@ class CallApi
         if (isset($res->date->display)) {
             $work[ 'creationDate' ] = $res->date->display;
         }
-        $authorName = $res->authors[0]->name->fr;
         $resf = $curlService->generateAPiUrl('authors', $authorName);
         $res = $resf->hits->hits[ 0 ];
         $work[ 'authorName' ] = $authorName;
-        $work['authorApiId'] = $res->_id;
+        $work[ 'authorApiId' ] = $res->_id;
 //        $authors = $res->_source;
 //        $author[ 'name' ] = $authors->name->fr;
 //        $author[ 'birthday' ] = $authors->birth->display;
@@ -65,14 +92,30 @@ class CallApi
      * @param string $request
      * @return mixed
      */
-    public function searchResultsWork(string $request)
+    public function searchResultsWork (string $request)
     {
         $curlService = new CurlService();
 
         $resf = $curlService->generateAPiUrl('works', $request);
+        $count = 0;
         foreach ($resf->hits->hits as $hit) {
-            $images[]['image'] = $hit->_source->images[ 0 ]->urls->huge->url;
-            $images[]['id'] = $hit->_source->id;
+            if (isset($hit->_source->images[ 0 ]->urls->huge->url)) {
+                $images[ $count ][ 'image' ] = $hit->_source->images[ 0 ]->urls->huge->url;
+
+            }
+            if (isset($hit->_source->id)) {
+                $images[ $count ][ 'id' ] = $hit->_source->id;
+
+            }
+            if (isset($hit->_source->title->fr)) {
+                $images[ $count ][ 'title' ] = $hit->_source->title->fr;
+
+            }
+            if (isset($hit->_source->authors[ 0 ]->name->fr)) {
+                $images[ $count ][ 'authorName' ] = $hit->_source->authors[ 0 ]->name->fr;
+
+            }
+            $count++;
         }
 
         return $images;
@@ -82,7 +125,7 @@ class CallApi
      * @param string $request
      * @return array
      */
-    public function searchResultAuthor(string $request)
+    public function searchResultAuthor (string $request)
     {
         $curlService = new CurlService();
         $resf = $curlService->generateAPiUrl('authors', $request);
