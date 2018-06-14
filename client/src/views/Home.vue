fa8d7845f1b1bb3691112e0c71e63b7546ecccb0
nooneexpectme <erwann0danguy@gmail.com>
2018-06-14 10:17:40 +0200

    Add SourceSansPro

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,13 +1,48 @@
 <template>
     <section>
         <!-- Navbar -->
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="https://bulma.io">
+                        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+                    </a>
+
+                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+
         <!-- Header -->
-        <div class="large-header">
-            <div class="overlay"></div>
-        </div>
-        <!-- Search bar -->
+        <section class="header-banner">
+            <div class="banner">
+                <div class="overlay">
+                    <div class="container">
+                        <!-- Search bar -->
+                        <label class="label has-text-white is-size-5">Rechercher une œuvre</label>
+                        <div class="control is-large">
+                            <input
+                                type="text"
+                                class="input is-large has-text-centered search-input"
+                                placeholder="Rechercher une œuvre..."
+                                v-model="searchInput"
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
         <!-- Search results -->
-        {{ welcomeMessage }}
+        <section class="section">
+            <div class="container">
+                test
+            </div>
+        </section>
     </section>
 </template>
 
@@ -15,7 +50,7 @@
 export default {
     name: 'app',
     data: () => ({
-        welcomeMessage: 'Welcome'
+        searchInput: 'Hello from the other side'
     })
 }
 </script>
