65aac6f4c3f09dcad760cd0c5ec46c4e05b2d48f
nooneexpectme <erwann0danguy@gmail.com>
2018-06-14 18:25:38 +0200

    Update Home components

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,56 +1,36 @@
 <template>
     <section>
-        <!-- Navbar -->
-        <nav class="navbar" role="navigation" aria-label="main navigation">
-            <div class="container">
-                <div class="navbar-brand">
-                    <a class="navbar-item" href="https://bulma.io">
-                        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
-                    </a>
-
-                    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                        <span aria-hidden="true"></span>
-                    </a>
-                </div>
-            </div>
-        </nav>
-
-        <!-- Header -->
-        <section class="header-banner">
-            <div class="banner">
-                <div class="overlay">
-                    <div class="container">
-                        <!-- Search bar -->
-                        <label class="label has-text-white is-size-5">Rechercher une œuvre</label>
-                        <div class="control is-large">
-                            <input
-                                type="text"
-                                class="input is-large has-text-centered search-input"
-                                placeholder="Rechercher une œuvre..."
-                                v-model="searchInput"
-                            />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <nav-bar></nav-bar>
+        <!-- TODO: Comment faire pour que lorsque qu'on modifie dans HeaderSearch.vue ça modifie la valeur dans cette vue aussi ? -->
+        <header-search v-bind:search="searchInput"></header-search>
         
         <!-- Search results -->
         <section class="section">
             <div class="container">
-                test
+                <div class="columns is-multiline">
+                    <div class="column is-3" v-for="n in 12" :key=n>
+                        <artwork-card></artwork-card>
+                    </div>
+                </div>
             </div>
         </section>
     </section>
 </template>
 
 <script>
+import NavBar from '../components/NavBar.vue'
+import HeaderSearch from '../components/home/HeaderSearch.vue'
+import ArtworkCard from '../components/ArtworkCard.vue'
+
 export default {
     name: 'app',
     data: () => ({
         searchInput: 'Hello from the other side'
-    })
+    }),
+    components: {
+        NavBar,
+        HeaderSearch,
+        ArtworkCard
+    }
 }
 </script>
